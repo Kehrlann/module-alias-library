f9a248524adf6ebcca9f8c61451c75f1df59c33d
Daniel Garnier-Moiroux <git@garnier.wf>
2019-12-02 13:59:59 +0100

    Create nested library

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 require("module-alias/register");
 console.log("Hello !");
+console.log("");
+
 console.log("---");
+console.log("Using module-alias from end-user code ...");
 const userModule = require("@deep/module");
 console.log(userModule.message);
+console.log("");
+
+console.log("---");
+console.log("Using module-alias from library ...");
+console.log(require("module-alias-nested-library"));
